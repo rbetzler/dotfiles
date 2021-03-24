422f101ea2a5a7d17cb7cd51cd89dca087b77ba5
rbetzler <rick.betzler@petalcard.com>
2021-03-24 11:02:27 -0400

    Adding dbt autocomplete

--- install_pi.sh
+++ install_pi.sh
@@ -9,3 +9,8 @@ apt-get install ctags
 # Clone vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Install dbt autocomplete
+curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/dbt-completion.bash > ~/.dbt-completion.bash
+# TODO: Move bash_profile to dotfiles
+echo 'source ~/.dbt-completion.bash' >> ~/.bash_profile
+
