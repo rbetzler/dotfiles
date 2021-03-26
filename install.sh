8c3e5231ce86d5d8ecaca9cff9ff68d919a2fb9e
rbetzler <rick.betzler@petalcard.com>
2021-03-25 21:34:04 -0400

    Adding generic install file

---
+++ install.sh
@@ -0,0 +1,5 @@
+
+# dbt autocomplete
+curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/_dbt > ~/_dbt
+curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/dbt-completion.bash > ~/dbt-completion.bash
+
