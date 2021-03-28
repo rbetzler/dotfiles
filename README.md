267f0839af3c6f62a83183990d9e83c2dd79cf15
rbetzler <rick.betzler@petalcard.com>
2021-03-28 16:55:03 +0100

    Refactoring install

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 My `$HOME`
 
-Pi install: `sudo sh ./install_pi.sh`
+Install: `sh install.sh`
 
-Apps:
-* atom
-  * Install app
-  * Then get packages:
-    * highlight-line
-    * highlight-selected
-    * linter-flake8
-    * project-manager
-* psql
-* dbeaver
--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 
-# dbt autocomplete
-mkdir ~/dbt-autocomplete
-curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/_dbt > ~/dbt-autocomplete/_dbt
-curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/dbt-completion.bash > ~/dbt-autocomplete/.dbt-completion.bash
+# Install zsh
+apt-get install zsh
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Switch default shell to zsh
+chsh -s $(which zsh)
+
+# Install vim-gtk
+apt-get install vim-gtk
+
+# Install ctags
+apt-get install ctags
+
+# Clone vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+
+# Install dbt autocomplete
+mkdir ${HOME}/dbt-autocomplete
+curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/_dbt > ${HOME}/dbt-autocomplete/_dbt
+curl https://raw.githubusercontent.com/fishtown-analytics/dbt-completion.bash/master/dbt-completion.bash > ${HOME}/dbt-autocomplete/.dbt-completion.bash
 
