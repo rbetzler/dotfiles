023b56939d4d769df6bdda2ebfb23f2cf2a37fb7
rbetzler <rbetzler94@gmail.com>
2021-07-10 20:34:51 -0400

    Updating install shell script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Install zsh
 apt-get install zsh
@@ -20,26 +21,11 @@ apt-get install hub
 # Install cmatrix, for fun
 apt-get install cmatrix
 
-# Install bat
-apt-get install bat  # This requires a manual install
-
-# This did not work but I do not care
-## Install procs
-#apt-get install procs
-
-# This did not work but I do not care
-## Install fd
-#apt-get install fd
-
-# Install exa
-apt-get install exa  # Need to fix this
-
 # Install figlet
 apt-get install figlet
 
-# Install vgrep
-# Check if this is preinstalled on ubuntu
-# apt-get install vgrep
+# Install vgrep (and other stuff)
+apt-get install atfs
 
 # Clone vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
@@ -57,3 +43,18 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Clone zsh autocomplete repo
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install bat
+wget https://github.com/sharkdp/bat/releases/download/v0.18.1/bat-musl_0.18.1_amd64.deb -O $HOME/Downloads/bat.deb
+dpkg -i $HOME/Downloads/bat.deb
+rm $HOME/Downloads/bat.deb
+
+# Install exa
+wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip -O $HOME/Downloads/exa.zip
+mkdir $HOME/Downloads/exa
+unzip $HOME/Downloads/exa.zip -d $HOME/Downloads/exa
+cp -r $HOME/Downloads/exa/bin/. /usr/local/bin/
+cp -r Downloads/exa/man/. /usr/share/man/man1/
+cp $HOME/Downloads/exa/completions/exa.zsh /usr/local/share/zsh/site-functions/exa.zsh
+rm -rf $HOME/Downloads/exa
+rm $HOME/Downloads/exa.zip
