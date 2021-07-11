34d49ed9356af8aa1d2fc15b7ebed2bd3183cd34
rbetzler <rbetzler94@gmail.com>
2021-07-10 21:32:43 -0400

    Another pass at manual installs

--- install.sh
+++ install.sh
@@ -16,16 +16,13 @@ apt-get install vim-gtk
 apt-get install ctags
 
 # Install hub
-apt-get install hub
+apt-get install hub  # Still need to sort this out
 
 # Install cmatrix, for fun
 apt-get install cmatrix
 
 # Install figlet
 apt-get install figlet
-
-# Install vgrep (and other stuff)
-apt-get install atfs
 
 # Clone vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
@@ -58,3 +55,17 @@ cp -r Downloads/exa/man/. /usr/share/man/man1/
 cp $HOME/Downloads/exa/completions/exa.zsh /usr/local/share/zsh/site-functions/exa.zsh
 rm -rf $HOME/Downloads/exa
 rm $HOME/Downloads/exa.zip
+
+# Install go (dependency of vgrep)
+wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz -O $HOME/Downloads/go.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf $HOME/Downloads/go.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Install vgrep
+# Might need to back up a few commits
+# (given the head of master was busted in 2021-07)
+git clone git@github.com:vrothberg/vgrep.git
+cd vgrep
+make build GO=/usr/local/go/bin/go
+make install GO=/usr/local/go/bin/go
