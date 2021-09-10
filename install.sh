206c1a8bdc7040835f300c8c41a127ba354a21b5
rbetzler <rbetzler94@gmail.com>
2021-09-10 07:05:23 -0400

    Adding tre as an alternative to tree
    Hopefully this is a good addition

--- install.sh
+++ install.sh
@@ -108,6 +108,9 @@ curl https://sh.rustup.rs -sSf | sh
 # Install sd
 cargo install sd
 
+# Install tree alternative
+cargo install tre-command
+
 # Install gh
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
