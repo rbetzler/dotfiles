5a3c70a16d180b425ec7b81668f51c4fca084e6c
rbetzler <rbetzler94@gmail.com>
2021-09-27 12:32:49 -0400

    Ipython exit config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -2,6 +2,9 @@
 # Call pdb after any exception
 c.InteractiveShell.pdb = True
 
+# Do not ask to confirm CTRL-D exit
+c.TerminalInteractiveShell.confirm_exit = False
+
 # Run lines on startup
 c.TerminalIPythonApp.exec_lines = [
     "print('\\n (╯°□°)╯︵ ┻━┻')",
