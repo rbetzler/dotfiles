b19d71cca9727fca0f90a9f0e9faeee401749fa6
rbetzler <rbetzler94@gmail.com>
2022-09-24 09:37:10 -0400

    Adding kubectl, helm installs

--- install.sh
+++ install.sh
@@ -244,6 +244,15 @@ cd ..
 curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 chmod +x /usr/local/bin/argocd
 
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 # Install pulumi, setup autocomplete
 curl -fsSL https://get.pulumi.com | sh
 pulumi gen-completion zsh >> "$HOME/.oh-my-zsh/custom/plugins/pulumi/pulumi.plugin.zsh"
