bca6e12fc533c0d87fbe7deae4f609d1bad41e19
rbetzler <rbetzler94@gmail.com>
2022-12-19 15:57:08 -0500

    Fix docker permissions
    Heavy handed but works.

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo chmod 777 /var/run/docker.sock
 
 # Install postgres and related tooling
 # Do not forget, `sudo systemctl disable postgresql`
