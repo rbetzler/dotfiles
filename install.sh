de1034b70015f6cbf6fdbdcd5024505305e6de2f
rbetzler <rbetzler94@gmail.com>
2022-12-19 15:36:05 -0500

    Add aws cli install

--- install.sh
+++ install.sh
@@ -223,6 +223,11 @@ install minikube-linux-amd64 /usr/local/bin/minikube
 curl -fsSL https://get.pulumi.com | sh
 pulumi gen-completion zsh >> "$HOME/.oh-my-zsh/custom/plugins/pulumi/pulumi.plugin.zsh"
 
+# Install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 #######
 # Other
 #######
