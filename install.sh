67c64f6a94fb35046b5a41aa62791e5dab26ebcf
rbetzler <rbetzler94@gmail.com>
2023-02-02 19:21:05 -0500

    Add kubeadm, kubelet
    To resolve `[ERROR CRI]: container runtime is not running: output`
    ```
    sudo rm /etc/containerd/config.toml
    sudo systemctl restart containerd.service
    ```
    To resolve `[kubelet-check] It seems like the kubelet isn't running or healthy.`
    ```
    sudo swapoff -a
    ```
    To get nodes working
    ```
    kubectl apply -f https://docs.projectcalico.org/manifests/calico-typha.yaml
    kubectl taint node --all node.kubernetes.io/not-ready:NoSchedule-
    kubectl taint node --all node-role.kubernetes.io/control-plane:NoSchedule-
    ```

--- install.sh
+++ install.sh
@@ -214,6 +214,12 @@ chmod +x /usr/local/bin/argocd
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# Install kubelet, kubeadm
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get install -y kubeadm kubelet
+
 # Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
