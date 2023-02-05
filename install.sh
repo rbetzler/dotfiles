da996cfdbaf8c44fd4f018783b4254b2d27ac9d9
rbetzler <rbetzler94@gmail.com>
2023-02-05 13:01:15 -0500

    Adding recording, viewing apps
    I will not become a podcaster. This is a necessary evil.

--- install.sh
+++ install.sh
@@ -88,6 +88,11 @@ sudo apt-get install -y \
   default-jre \
   go-md2man
 
+# Install obs studio for screencasting, vlc for viewing
+sudo add-apt-repository ppa:obsproject/obs-studio && \
+  sudo apt update && \
+  sudo apt install -y ffmpeg obs-studio vlc
+
 # Install gh
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
