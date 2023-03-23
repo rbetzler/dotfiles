88c55ad61dc64ed82b41eea74f4ef802eff04964
rbetzler <rbetzler94@gmail.com>
2023-03-23 09:02:51 -0400

    Clean up shell utils
    I'm itching to clean these up but I know I should do it slowly.
    Otherwise, I'll make a mess.

--- .utils/utils.sh
+++ .utils/utils.sh
@@ -24,17 +24,10 @@ else
   bat_cmd="bat"
 fi
 
-# Fuzzy commands
-# Template find command and ignore a few top level dirs
+# Return the selected file, like regular fzf
 __FD="fd --no-ignore -E backups -E dbt-autocomplete -E dracula -E google-cloud-sdk -E venv -E vgrep"
 __FZF_PREVIEW="if [ -d {} ]; then tree -C {} | head -200; else $bat_cmd --style=numbers --color=always --line-range :500 {}; fi"
-__FZF_PREVIEW_VFGREP=" n={3}; if (( \$n < 10 )); then m=0; else m=\$((\$n-5)); fi; bat {2} --style=numbers --color=always --highlight-line {3} --line-range \$m: "
-__FZF_PREVIEW_CTAGS="x={}; file_name=\$(echo \$x | awk '{print \$2}') ; line_start=\$(echo \$x | awk '{print \$3}' | awk -F';' '{print \$1}') ; tmp_window_start=\$((\$line_start - 2)) ; window_start=\$(( \$tmp_window_start > 0 ? \$tmp_window_start : 0 )) ; bat --color=always \$file_name --highlight-line \$line_start --line-range \$window_start:"
-# __FZF_BIND='f8:execute(terminator --new-tab {})'
 __FZF_BIND='f8:execute(nvim {} < /dev/tty)'
-__FZF_BIND_CTAGS="f8:execute(nvim \$(echo {} | awk '{print \$2}'))"
-
-# Return the selected file, like regular fzf
 fzfd(){
   eval $__FD | fzf -m --height 60% --preview $__FZF_PREVIEW --bind $__FZF_BIND
 }
@@ -54,6 +47,7 @@ vf(){
 
 # fzf-vgrep then open in nvim
 # If an arg is provided, then do not ignore hidden dirs and pass in the search string
+__FZF_PREVIEW_VFGREP=" n={3}; if (( \$n < 10 )); then m=0; else m=\$((\$n-5)); fi; bat {2} --style=numbers --color=always --highlight-line {3} --line-range \$m: "
 vfgrep() {
   if [ -z "$1" ]; then
     include_hidden_files=""
@@ -62,30 +56,12 @@ vfgrep() {
     include_hidden_files="--no-git"
     search_arg="$1"
   fi
-  files=$(vgrep --no-header $include_hidden_files "$search_arg" | fzf -m --height 60% --preview $__FZF_PREVIEW_VFGREP --bind "change:reload:vgrep $include_hidden_files --no-header {q} || true" --ansi --phony --tac | \
+  files=$(vgrep --no-header $include_hidden_files "$search_arg" | \
+    fzf -m --height 60% --preview $__FZF_PREVIEW_VFGREP --bind "change:reload:vgrep $include_hidden_files --no-header {q} || true" --ansi --phony --tac | \
     awk '{if (NR==1) {print("+" $3, $2)} else {print("+\"tabnew +" $3, $2 "\"")}}' ORS=' ')
   if [[ "$files" != "" ]]; then
     eval 'nvim' "$files"
   fi
-}
-
-sdgrep(){
-  query=$(vgrep --no-header | \
-    fzf --height 60% --bind "change:reload:vgrep --no-header {q} || true" --ansi --phony --tac --print-query | \
-    awk 'NR==1 {print}'
-  )
-  while true; do
-    print -n -P "%F{cyan}%BReplacement: "
-    read replacement
-    sd "$query" "$replacement" $(fd --type f)
-    git diff
-    print -n -P "%F{cyan}%BAccept? (Y/N): "
-    read accept
-    case $accept in
-      [Yy]* ) break ;;
-      [Nn]* ) git checkout . 2>/dev/null ;;
-    esac
-  done
 }
 
 hist(){
@@ -106,7 +82,7 @@ djson(){
   selection="$(cat "$1" | jq '.nodes | keys' | sed 's/,$//g' | awk '{if(length>1 && $1=$1) {print}}' | fzf --preview "bat $1 | jq -C '.nodes[\"{}\"]'")"
   if [ -z "$selection" ]; then
     echo 'No selection'
-  else;
+  else
     bat "$1" | jq -C ".nodes[$selection]" | less
   fi
 }
@@ -116,6 +92,8 @@ hcat(){
   python -c 'import sys, pandas as pd; pd.set_option("display.max_columns", None); pd.set_option("display.max_rows", None); print(pd.read_csv(sys.argv[1], nrows=3).head(1).T)' "$1"
 }
 
+__FZF_PREVIEW_CTAGS="x={}; file_name=\$(echo \$x | awk '{print \$2}') ; line_start=\$(echo \$x | awk '{print \$3}' | awk -F';' '{print \$1}') ; tmp_window_start=\$((\$line_start - 2)) ; window_start=\$(( \$tmp_window_start > 0 ? \$tmp_window_start : 0 )) ; bat --color=always \$file_name --highlight-line \$line_start --line-range \$window_start:"
+__FZF_BIND_CTAGS="f8:execute(nvim \$(echo {} | awk '{print \$2}'))"
 ftags(){
   cat $HOME/.ctags/tags | fzf --preview $__FZF_PREVIEW_CTAGS --bind $__FZF_BIND_CTAGS
 }
