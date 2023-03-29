4119cf6b718a68ec5227e3a91895a6481433cf6a
rbetzler <rbetzler94@gmail.com>
2023-03-29 11:42:12 -0400

    Update argo cd install

--- install.sh
+++ install.sh
@@ -247,9 +247,10 @@ rm $HOME/Downloads/pandoc.deb
 # Devops tools
 ##############
 
-# Install argo cli
-curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-chmod +x /usr/local/bin/argocd
+# Install argo cd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
