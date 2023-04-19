03c4c0b54f6da22e955814e5c86aa36e979ff223
rbetzler <rbetzler94@gmail.com>
2023-04-19 12:13:06 -0400

    Adding redis install

--- install.sh
+++ install.sh
@@ -78,6 +78,12 @@ sudo apt-get install chromium-browser
 # Install miscellaneous
 sudo apt-get install -y copyq preload unzip blueman htop xkbset screen pwgen
 
+# Install redis
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis
+
 # Install autojump
 sudo apt-get install -y \
   autojump \
