154276fd34d997ec889b17152b012510071ae7e4
rbetzler <rbetzler94@gmail.com>
2023-05-12 15:13:58 -0400

    Adding vault install

--- install.sh
+++ install.sh
@@ -317,6 +317,11 @@ go install -tags extended github.com/gohugoio/hugo@latest
 hugo completion zsh > $HOME/.utils/hugo.sh
 sed -i '1i compdef _hugo hugo' $HOME/.utils/hugo.sh
 
+# Install vault cli
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vault
+
 #######
 # Other
 #######
