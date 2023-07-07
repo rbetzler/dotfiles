f1ab3bfb7f5d1b31e3159561db5aec0dcf7bb02e
rbetzler <rbetzler94@gmail.com>
2023-07-07 09:37:49 -0400

    Fix minor install issues for docker

--- install.sh
+++ install.sh
@@ -17,11 +17,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt-get install terminator
 
 # Placeholder for docker, docker-compose
-mkdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo chmod 777 /var/run/docker.sock
 
