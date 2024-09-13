ce85e2b6275f3fb6c08eaf9079b2184746f7d4f8
rbetzler <rbetzler94@gmail.com>
2024-09-13 07:51:32 -0400

    Update spotify install

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ mkdir -p ~/.local/share/gnome-shell/extensions/
 unzip $HOME/Downloads/wintile@nowsci.com.zip -d .local/share/gnome-shell/extensions/wintile@nowsci.com/
 
 # Add spotfiy install
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
