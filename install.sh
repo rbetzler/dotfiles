7da1a2a69b7cb9c7d5b673102fdc19796979f3c3
rbetzler <rbetzler94@gmail.com>
2024-09-13 07:52:44 -0400

    Swap firefox for zen
    Had to do some manual stuff

--- install.sh
+++ install.sh
@@ -371,6 +371,13 @@ curl -LO https://github.com/git-lfs/git-lfs/releases/download/v3.4.1/git-lfs-lin
 tar -xf git-lfs-linux-amd64-v3.4.1.tar.gz
 sudo bash git-lfs-3.4.1/install.sh
 
+# Install zen browser
+wget https://github.com/zen-browser/desktop/releases/download/1.0.0-a.39/zen.linux-specific.tar.bz2 -O $HOME/Downloads/zen.tar.bz2
+tar xjf $HOME/Downloads/zen.tar.bz2
+sudo mv zen /opt/
+# Grab desktop icon manually
+curl https://cdn.jsdelivr.net/gh/zen-browser/www/public/logos/zen-black.svg | sudo tee /usr/share/icons/zen-black.svg
+
 ##################
 # Install crontabs
 ##################
