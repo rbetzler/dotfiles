1dd7bb5a39c5085c1311d7ff7dcfcf42f900e85c
rbetzler <rbetzler94@gmail.com>
2024-10-18 09:02:51 -0400

    Accept yay prompts

--- .installers/03_yay.sh
+++ .installers/03_yay.sh
@@ -8,13 +8,13 @@ cd "$HOME/repos/yay/"
 makepkg -si
 
 # Install aur packages
-yay google-cloud-cli
-yay google-cloud-cli-gke-gcloud-auth-plugin
-yay slack-desktop
-yay spotify
-yay zoom
-yay dracula-gtk-theme
-yay dracula-icons-git
+yay --sync --noconfirm google-cloud-cli
+yay --sync --noconfirm google-cloud-cli-gke-gcloud-auth-plugin
+yay --sync --noconfirm slack-desktop
+yay --sync --noconfirm spotify
+yay --sync --noconfirm zoom
+yay --sync --noconfirm dracula-gtk-theme
+yay --sync --noconfirm dracula-icons-git
 
 # Also in nix
 # yay autojump-rs
@@ -22,4 +22,4 @@ yay dracula-icons-git
 # yay vgrep
 
 # Maybe
-yay zramswap
+yay --sync --noconfirm zramswap
