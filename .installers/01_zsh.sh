2832e3022b5b2ff044d08fe75331ff070b7af3bc
rbetzler <rbetzler94@gmail.com>
2024-10-18 09:03:00 -0400

    Reset zshrc postinstall

--- .installers/01_zsh.sh
+++ .installers/01_zsh.sh
@@ -10,3 +10,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Symbolic link for zsh theme
 ln -s "${HOME}/dracula/zsh/dracula.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/dracula.zsh-theme"
+
+# Reset zshrc
+git checkout "$HOME/.zshrc"
