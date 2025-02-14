939d8b2089da3b8467d1c7651befb85381d48072
rbetzler <rbetzler94@gmail.com>
2025-02-14 09:05:23 -0500

    Move oh my zsh into submod
    Rather than bash, and rather than yay, move this into a git submodule
    where I can specify the correct directory and not need to worry about
    idempotency when running a bash script

--- install.sh
+++ install.sh
@@ -4,13 +4,8 @@ set -euxo pipefail
 
 reqs="git zsh"
 for req in $reqs; do
-  if which $req > /dev/null; then
-    echo "$req installed"
-  else
-    echo "$req not installed, installing now"
-    if grep -q Arch /etc/os-release; then
-      sudo pacman --sync --noconfirm $req
-    fi
+  if grep -q Arch /etc/os-release; then
+    sudo pacman --sync --noconfirm $req
   fi
 done
 
@@ -19,9 +14,6 @@ git remote add https https://github.com/rbetzler/dotfiles.git
 git fetch --all
 rm "$HOME/.bashrc"
 git checkout arch
-
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Init, update submodules
 git submodule update --init --recursive
