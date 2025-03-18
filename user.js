8fbe9e2da7b7ddb3ab70a8ecaba7e1b627eb01e2
rbetzler <rbetzler94@gmail.com>
2025-03-17 20:54:02 -0400

    Add zen user.js
    Hopefully this plus a manual step to add bitwarden eases my browser pain

---
+++ user.js
@@ -0,0 +1,8 @@
+
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
+user_pref("browser.warnOnQuitShortcut", false);
+user_pref("signon.rememberSignons", false);
+user_pref("zen.view.show-newtab-button-top", false);
