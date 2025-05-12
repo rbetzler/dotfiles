4c361347e2ef57ae83566e30dc12020b238a0863
rbetzler <rbetzler94@gmail.com>
2025-05-12 15:12:20 -0400

    Tweak monitor attacher

--- .bin/hyprland_monitor_attacher.sh
+++ .bin/hyprland_monitor_attacher.sh
@@ -2,8 +2,14 @@
 #
 # Toggle monitors based on what is plugged in
 
-num=$(hyprctl monitors all | grep -c Monitor)
-if [[ $num == 3 ]]; then
+restart_waybar(){
+  killall waybar
+  hyprctl dispatch exec waybar
+}
+
+monitors=$(hyprctl monitors -j | jq '.[]."name"')
+if echo "$monitors" | grep --quiet 'DP-1'; then
+if echo "$monitors" | grep --quiet 'DP-3'; then
   hyprctl keyword monitor "eDP-1, disable"
   hyprctl keyword monitor "DP-1, 1920x1080, 1920x0, 1"
   hyprctl keyword monitor "DP-3, 2560x1440, 3840x0, 1"
@@ -13,8 +19,16 @@ if [[ $num == 3 ]]; then
   hyprctl dispatch moveworkspacetomonitor 4 DP-3
   hyprctl dispatch moveworkspacetomonitor 5 DP-3
   hyprctl dispatch moveworkspacetomonitor 6 DP-3
-
-  # Restart waybar
-  killall waybar
-  hyprctl dispatch exec waybar
+  restart_waybar
+else
+  hyprctl keyword monitor "eDP-1, disable"
+  hyprctl keyword monitor "DP-1, 1920x1080, 1920x0, 1"
+  hyprctl dispatch moveworkspacetomonitor 1 DP-1
+  hyprctl dispatch moveworkspacetomonitor 2 DP-1
+  hyprctl dispatch moveworkspacetomonitor 3 DP-1
+  hyprctl dispatch moveworkspacetomonitor 4 DP-1
+  hyprctl dispatch moveworkspacetomonitor 5 DP-1
+  hyprctl dispatch moveworkspacetomonitor 6 DP-1
+  restart_waybar
+fi
 fi
