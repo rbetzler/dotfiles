f90195ea5d9b2ce23e57284cf8a0db6076f3e759
rbetzler <rbetzler94@gmail.com>
2025-06-03 17:28:01 -0400

    Tweak nvim bindings
    Not fully satisified tbh

--- .config/nvim/lua/bindings.lua
+++ .config/nvim/lua/bindings.lua
@@ -12,6 +12,8 @@ keymap("i", "<C-v>", "<Esc>P", opts)
 -- Match visual selector vim-native bindings with arrow keys
 vim.keymap.set("n", "<C-k>", "<Plug>(VM-Add-Cursor-Up)", opts)
 vim.keymap.set("n", "<C-j>", "<Plug>(VM-Add-Cursor-Down)", opts)
+vim.keymap.set("n", "<C-Up>", "<Plug>(VM-Add-Cursor-Up)", opts)
+vim.keymap.set("n", "<C-Down>", "<Plug>(VM-Add-Cursor-Down)", opts)
 
 -- Normal mode navigation mappings
 keymap("n", "<leader><C-h>", "<CMD>wincmd h<CR>", opts)
@@ -19,27 +21,34 @@ keymap("n", "<leader><C-j>", "<CMD>wincmd j<CR>", opts)
 keymap("n", "<leader><C-k>", "<CMD>wincmd k<CR>", opts)
 keymap("n", "<leader><C-l>", "<CMD>wincmd l<CR>", opts)
 
+-- Create splits
 vim.keymap.set("n", "<leader><C-S-h>", function()
 	vim.cmd("vsplit")
 	vim.cmd("wincmd l")
 	vim.cmd("wincmd H")
-end, { noremap = true, silent = true })
+end, opts)
 
 vim.keymap.set("n", "<leader><C-S-j>", function()
 	vim.cmd("split")
 	vim.cmd("wincmd j")
-end, { noremap = true, silent = true })
+end, opts)
 
 vim.keymap.set("n", "<leader><C-S-k>", function()
 	vim.cmd("split")
 	vim.cmd("wincmd j")
 	vim.cmd("wincmd K")
-end, { noremap = true, silent = true })
+end, opts)
 
 vim.keymap.set("n", "<leader><C-S-l>", function()
 	vim.cmd("vsplit")
 	vim.cmd("wincmd l")
-end, { noremap = true, silent = true })
+end, opts)
+
+-- Resize splits
+vim.keymap.set('n', '<C-S-h>', '<CMD>vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-S-j>', '<CMD>resize +2<CR>', opts)
+vim.keymap.set('n', '<C-S-k>', '<CMD>resize -2<CR>', opts)
+vim.keymap.set('n', '<C-S-l>', '<CMD>vertical resize +2<CR>', opts)
 
 -- Random
 keymap("n", "<leader><C-a>", "<CMD>NERDTreeFocus<CR>", opts)
