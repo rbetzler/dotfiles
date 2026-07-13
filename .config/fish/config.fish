fe7b4ecb5ba77f892abf8889e54954b726b7e916
rbetzler <rbetzler94@gmail.com>
2026-07-13 15:01:52 -0400

    Disable fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 set -gx STARSHIP_CACHE ~/.cache/starship/logs/fish
 set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
 
+set -U fish_greeting ""
+
 if status is-interactive
     starship init fish | source
 end
